b63a1f4c171ea47eb37e4228f3b7855d53947c4b
Nicolas Garneau <ngarneau@ngarneau.com>
2012-05-28 19:36:12 -0400

    adding KoSolr classes and index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    include_once('bootstrap.php');
+    $instance = KoSolr::getInstance();
+    $server = $instance->getServer();
+?>
+<html>
+	<head>
+        <title>Solr</title>
+    </head>
+    <body>
+        <h1>Solr</h1>
+    </body>
+</html>
